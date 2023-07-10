9a70e4d22284bd4899a65178a76cafa50349bea0
nekita <nn-nekrasov2019@yandex.ru>
2023-07-10 16:06:29 +0300

    сохраняю

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+// int numA = 6;
+// int numB = 4;
+// int max = numA;
+// if (numA < numB)
+// {
+//     max = numB;
+// }
+// Console.WriteLine(max);
+
+//Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+
+// int num1 = 10;
+// int num2 = 6;
+// int num3 = 9;
+// int max = num1;
+// if (num1 < num2){
+//     max=num2;
+// }
+// if (num2 < num3){
+//     max=num3;
+// }
+// if (num1 > num3){
+//     max = num1;
+// }
+// Console.WriteLine(max);
+
+//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным
+
+// Console.Write("Введите число: ");  
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 1){
+//     Console.Write("Нечетное");
+// }
+// else {
+//     Console.Write("Четное");
+// }
+
+//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int num = 1;
+// while (num <= N){
+//     if (num % 2 == 0){
+//         Console.Write(num + " ");
+//     }
+//     num = num+1;
+// }
